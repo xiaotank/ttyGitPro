bcff1868ace706963b0a4f191f49d35da8c7099e
xiaotank <329873423@qq.com>
2016-07-05 18:40:03 +0800

    Update ValuesController.cs

--- MvcGit/Controllers/ValuesController.cs
+++ MvcGit/Controllers/ValuesController.cs
@@ -12,7 +12,7 @@ namespace MvcGit.Controllers
         // GET api/valuestty
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "value1", "value211" };
         }
 
         // GET api/values/5
